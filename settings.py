5f4860561d93b64068e6f66e8daf2c29dd534a65
Tam <t.dimairo@yahoo.com>
2022-12-07 10:53:41 +1300

    Update settings.py

--- settings.py
+++ settings.py
@@ -154,7 +154,7 @@ REST_FRAMEWORK = {
     'DEFAULT_THROTTLE_RATES': {
         'user': '5/second'
     },
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'api.pagination.CustomLimitOffsetPagination',
     'PAGE_SIZE': 500
 }
 
